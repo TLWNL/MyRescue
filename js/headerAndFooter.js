ffff4c3d288e49bcad1f17e7b5cf5df54e716493
Dylan <dylanrijnsburger@gmail.com>
2021-08-17 16:19:59 +0200

    Kanker

--- js/headerAndFooter.js
+++ js/headerAndFooter.js
@@ -1,24 +1,220 @@
-const header = document.createElement('header');
-header.innerHTML =
-    <h1>
-    </h1>
 
+// const header = document.createElement('header');
+// header.innerHTML = 
+//     <div class="demo">
+//         <div class="container">
+//             <div class="row">
+//                 <div class="col-md-12">
+//                     <nav class="navbar navbar-default navbar-mobile bootsnav on">
+//                         <div class="navbar-header">
+//                             <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-menu">
+//                                 <i class="fa fa-bars"></i>
+//                             </button>
+//                         </div>
+//                         <div class="collapse navbar-collapse" id="navbar-menu">
+//                             <ul class="nav navbar-nav" data-in="fadeInDown" data-out="fadeOutUp">
+//                                 <li class="active"><a href="#" data-hover="Home">Home <span data-hover="Home"></span></a></li>
+//                                 <li><a href="#" data-hover="About Us">About us <span data-hover="About"></span></a></li>
+//                                 <li class="dropdown">
+//                                     <a href="#" class="dropdown-toggle" data-toggle="dropdown" data-hover="Shortcodes">Shortcodes <span data-hover="Shortcodes"></span></a>
+//                                     <ul class="dropdown-menu animated">
+//                                         <li><a href="#">Custom Menu</a></li>
+//                                         <li><a href="#">Custom Menu</a></li>
+//                                         <li class="dropdown">
+//                                             <a href="#" class="dropdown-toggle" data-toggle="dropdown">Sub Menu</a>
+//                                             <ul class="dropdown-menu animated">
+//                                                 <li><a href="#">Custom Menu</a></li>
+//                                                 <li><a href="#">Custom Menu</a></li>
+//                                                 <li class="dropdown">
+//                                                     <a href="#" class="dropdown-toggle" data-toggle="dropdown">Sub Menu</a>
+//                                                     <ul class="dropdown-menu multi-dropdown animated">
+//                                                         <li><a href="#">Custom Menu</a></li>
+//                                                         <li><a href="#">Custom Menu</a></li>
+//                                                         <li><a href="#">Custom Menu</a></li>
+//                                                         <li><a href="#">Custom Menu</a></li>
+//                                                     </ul>
+//                                                 </li>
+//                                                 <li><a href="#">Custom Menu</a></li>
+//                                             </ul>
+//                                         </li>
+//                                         <li><a href="#">Custom Menu</a></li>
+//                                         <li><a href="#">Custom Menu</a></li>
+//                                         <li><a href="#">Custom Menu</a></li>
+//                                         <li><a href="#">Custom Menu</a></li>
+//                                     </ul>
+//                                 </li>
+//                                 <li class="dropdown">
+//                                     <a href="#" class="dropdown-toggle" data-toggle="dropdown" data-hover="Animals">Animals <span data-hover="Pages"></span></a>
+//                                     <ul class="dropdown-menu animated">
+//                                     <li><a href="#">Bears</a></li>
 
-const footer = document.createElement('footer');
-footer.innerHTML = 
-    <h1>
+//                                     <li><a href="#">Bobcat</a></li>
 
-    </h1>
+//                                     <li><a href="#">Lynx</a></li>
 
-function appendHeaderAndFooter() {
-    appendHeader();
-    appendFooter();
-}
+//                                     <li><a href="#">Moose</a></li>
 
-function appendHeader() {
-    document.body.appendChild(header.content);
-}
+//                                     <li><a href="#">White Tail Deer</a></li>
 
-function appendFooter() {
-    document.body.appendChild(footer.content);
-}
+//                                     <li><a href="#">Bobcat</a></li>
+
+//                                     <li><a href="#">Opossum</a></li>
+
+//                                     <li><a href="#">Porcupine</a></li>
+
+//                                     <li><a href="#">Fisher</a></li>
+
+//                                     <li><a href="#">American Marten</a></li>
+
+//                                     <li><a href="#">Beaver</a></li>
+
+//                                     <li><a href="#">Osprey</a></li>
+
+//                                     <li><a href="#">Adirondack Loons</a></li>
+                                    
+//                                     <li><a href="#">Ravens Crows & Wolves</a></li>
+
+//                                     <li class="dropdown">
+//                                         <a href="#" class="dropdown-toggle" data-toggle="dropdown">Birds</a>
+//                                         <ul class="dropdown-menu animated">
+//                                         <li><a href="#">Snowy Owl</a></li>
+//                                         <li><a href="#">Eurasian Eagle Owl</a></li>
+//                                         <li><a href="#">Barred Owl</a></li>
+//                                         <li><a href="#">Great Horned Owl</a></li>
+//                                         <li><a href="#">Great Grey Owl</a></li>
+//                                         <li><a href="#">Schreeched Owl</a></li>
+//                                         <li><a href="#">Saw Whet Owl</a></li>
+//                                         <li><a href="#">Barn Owl</a></li>
+//                                         <li><a href="#">Long Eared Owl</a></li>
+//                                         <li><a href="#">Short Eared Owl</a></li>
+//                                         </ul>
+//                                     </li>
+
+//                                     <li class="dropdown">
+//                                         <a href="#" class="dropdown-toggle" data-toggle="dropwdown">MLM</a>
+//                                         <ul class="dropdown-menu multi-level" role="menu" aria-labelledby="dropdownMenu">
+//                                         <li><a href="#">Some action</a></li>
+//                                         <li><a href="#">Some other action</a></li>
+//                                         <li class="divider"></li>
+//                                         <li class="dropdown-submenu">
+//                                             <a tabindex="-1" href="#">Hover me for more options</a>
+//                                             <ul class="dropdown-menu">
+//                                             <li><a tabindex="-1" href="#">Second level</a></li>
+//                                             <li class="dropdown-submenu">
+//                                                 <a href="#">Even More..</a>
+//                                                 <ul class="dropdown-menu">
+//                                                     <li><a href="#">3rd level</a></li>
+//                                                 <li><a href="#">3rd level</a></li>
+//                                                 </ul>
+//                                             </li>
+//                                             <li><a href="#">Second level</a></li>
+//                                             <li><a href="#">Second level</a></li>
+//                                             </ul>
+//                                         </li>
+//                                         </ul>
+//                                     </li>
+                                                                    
+//                                     <li class="dropdown">
+//                                         <a href="#" class="dropdown-toggle" data-toggle="dropdown">Foxes</a>
+//                                         <ul class="dropdown-menu animated">
+//                                             <li><a href="#">Red Fox</a></li>
+//                                             <li><a href="#">Grey Fox</a></li>
+//                                             <li><a href="#">Artic Fox</a></li>
+//                                         </ul>
+//                                     </li>
+                                    
+//                                     <li class="dropdown">
+//                                         <a href="#" class="dropdown-toggle" data-toggle="dropdown">Hawkes</a>
+//                                         <ul class="dropdown-menu animated">
+//                                             <li><a href="#">Red Tailed Hawk</a></li>
+//                                             <li><a href="#">Broad Winged Hawk</a></li>
+//                                             <li><a href="#">Goshawk</a></li>
+//                                             <li><a href="#">Swainsons Hawk</a></li>
+//                                             <li><a href="#">Rough Legged Hawk</a></li>
+//                                         </ul>
+//                                     </li> 
+
+//                                     <li><a href="#">Northern Harrier</a></li>
+
+//                                     <li><a href="#">Merlin</a></li>
+
+//                                     <li><a href="#">Kestrel</a></li>
+
+                                    
+//                                     <li class="dropdown">
+//                                         <a href="#" class="dropdown-toggle" data-toggle="dropdown">Vultures</a>
+//                                         <ul class="dropdown-menu animated">
+//                                             <li><a href="#">Turkey Vulture</a></li>
+//                                             <li><a href="#">Black Vulture</a></li>
+//                                         </ul>
+//                                     </li>
+//                                     </ul>
+//                                 </li>
+//                                 <li><a href="#" data-hover="Portfolio">Portfolio <span data-hover="Portfolio"></span></a></li>
+//                                 <li class="dropdown megamenu-fw">
+//                                     <a href="#" class="dropdown-toggle" data-toggle="dropdown" data-hover="Megamenu">Megamenu <span data-hover="Megamenu"></span></a>
+//                                     <ul class="dropdown-menu megamenu-content animated" role="menu">
+//                                         <li>
+//                                             <div class="row">
+//                                                 <div class="col-menu col-md-3">
+//                                                     <h6 class="title">Title Menu One</h6>
+//                                                     <div class="content">
+//                                                         <ul class="menu-col">
+//                                                             <li><a href="#">Custom Menu</a></li>
+//                                                             <li><a href="#">Custom Menu</a></li>
+//                                                             <li><a href="#">Custom Menu</a></li>
+//                                                             <li><a href="#">Custom Menu</a></li>
+//                                                             <li><a href="#">Custom Menu</a></li>
+//                                                         </ul>
+//                                                     </div>
+//                                                 </div>
+//                                                 <div class="col-menu col-md-3">
+//                                                     <h6 class="title">Title Menu Two</h6>
+//                                                     <div class="content">
+//                                                         <ul class="menu-col">
+//                                                             <li><a href="#">Custom Menu</a></li>
+//                                                             <li><a href="#">Custom Menu</a></li>
+//                                                             <li><a href="#">Custom Menu</a></li>
+//                                                             <li><a href="#">Custom Menu</a></li>
+//                                                             <li><a href="#">Custom Menu</a></li>
+//                                                         </ul>
+//                                                     </div>
+//                                                 </div>
+//                                                 <div class="col-menu col-md-3">
+//                                                     <h6 class="title">Title Menu Three</h6>
+//                                                     <div class="content">
+//                                                         <ul class="menu-col">
+//                                                             <li><a href="#">Custom Menu</a></li>
+//                                                             <li><a href="#">Custom Menu</a></li>
+//                                                             <li><a href="#">Custom Menu</a></li>
+//                                                             <li><a href="#">Custom Menu</a></li>
+//                                                             <li><a href="#">Custom Menu</a></li>
+//                                                         </ul>
+//                                                     </div>
+//                                                 </div>
+//                                                 <div class="col-menu col-md-3">
+//                                                     <h6 class="title">Title Menu Four</h6>
+//                                                     <div class="content">
+//                                                         <ul class="menu-col">
+//                                                             <li><a href="#">Custom Menu</a></li>
+//                                                             <li><a href="#">Custom Menu</a></li>
+//                                                             <li><a href="#">Custom Menu</a></li>
+//                                                             <li><a href="#">Custom Menu</a></li>
+//                                                             <li><a href="#">Custom Menu</a></li>
+//                                                         </ul>
+//                                                     </div>
+//                                                 </div>
+//                                             </div>
+//                                         </li>
+//                                     </ul>
+//                                 </li>
+//                                 <li><a href="#" data-hover="Contact">Contact <span data-hover="Contact"></span></a></li>
+//                             </ul>
+//                         </div>
+//                     </nav>
+//                 </div>
+//             </div>
+//         </div>
+//     </div>
+//     ;
+// document.body.appendChild(header.content);
